db1cc49b87b3d8577b714ed9ca09461bc3e3dae5
jasonpark723 <32312897+jasonpark723@users.noreply.github.com>
2019-09-17 20:08:19 -0700

    Update settings.py

--- book_authors_proj/settings.py
+++ book_authors_proj/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '439vc+(6+sht%#@a_@bv*3!@1w^^n_gdf$lms@bz0=6(oe!rx6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
